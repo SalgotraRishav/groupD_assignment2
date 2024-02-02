9d7920d7245038095110db7b4e7799e30bea34cb
SalgotraRishav <salgotra_rishav@yahoo.in>
2024-02-01 19:48:18 -0500

    malhatras func code

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package groupD_assignment2
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Calling the function
+	greatestOfThree()
+}
+
+func greatestOfThree() {
+	// Prompt user for input
+	fmt.Println("Enter three numbers:")
+
+	// Taking three variables of integer type
+	var num1, num2, num3 int
+
+	// Read inputs from user and save in num1, num2 and num3 variables
+	fmt.Print("Number 1: ")
+	fmt.Scanln(&num1)
+	fmt.Print("Number 2: ")
+	fmt.Scanln(&num2)
+	fmt.Print("Number 3: ")
+	fmt.Scanln(&num3)
+
+	// Calling the 'greatest' function and storing the return value in num variable
+	num := greatest(num1, num2, num3)
+
+	// Displaying the output on the screen
+	fmt.Println(num, "is the greatest")
+}
+
+// function to calculate the greatest out of three
+func greatest(a, b, c int) int {
+	if a >= b && a >= c {
+		return a
+	} else if b >= a && b >= c {
+		return b
+	} else {
+		return c
+	}
 }
